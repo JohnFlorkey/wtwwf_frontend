27fd338608906c3d56892ea829919f167c146be3
John Florkey <john.florkey@gmail.com>
2021-10-30 12:03:36 -0400

    abstracted wtwwf api url

--- src/actions/movieSearchResults.js
+++ src/actions/movieSearchResults.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { MOVIE_SEARCH_RESULTS_LOAD } from "./types";
+import { WTWWF_API_URL } from "../utilities/config";
 
 export function getMovieSearchResults(title) {
   return async function (dispatch) {
     try {
-      const response = await axios.get("http://localhost:3002/search/movies");
+      const response = await axios.get(`${WTWWF_API_URL}/search/movies`);
       dispatch(movieSearchResultsLoad(response.data));
     } catch (err) {
       console.log(err);
